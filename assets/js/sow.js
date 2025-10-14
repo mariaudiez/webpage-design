560ef18bc846693d74243209a68467d5df488967
María Ugarte <m.ugartediez@gmail.com>
2025-10-15 00:08:49 +0200

    Tarjetas

---
+++ assets/js/sow.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    const headerTitle = document.querySelector("#headerTitle");
+    headerTitle.classList.add("active");
+  }, 1000);
+
+  const webCards = document.querySelectorAll(".web-card");
+
+  webCards.forEach(function (card) {
+    // Este bloque se ejecuta al hacer hover
+    card.addEventListener("mouseover", function () {
+      webCards.forEach(function (card) {
+        let title = card.querySelector(".web-card-title");
+        title.classList.remove("active");
+      });
+      let title = card.querySelector(".web-card-title");
+      title.classList.add("active");
+    });
+  });
+});
