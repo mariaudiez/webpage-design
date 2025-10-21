5fe4eca24ac905f607bf87358af2bfb046c92546
María Ugarte <m.ugartediez@gmail.com>
2025-10-21 22:55:23 +0200

    Nav bar + Info (corrección)

--- assets/js/sow.js
+++ assets/js/sow.js
@@ -2,31 +2,29 @@ document.addEventListener("DOMContentLoaded", function () {
   setTimeout(function () {
     const headerTitle = document.querySelector("#headerTitle");
     headerTitle.classList.add("active");
+
+    const background = document.querySelector(".white-cover");
+    background.classList.add("active");
   }, 1000);
 
   const webCards = document.querySelectorAll(".web-card");
 
   webCards.forEach(function (card) {
     // Este bloque se ejecuta al hacer hover
-    card.addEventListener("mouseover", function () {
-      webCards.forEach(function (card) {
-        let title = card.querySelector(".web-card-title");
-        title.classList.remove("active");
-        // let background = card.querySelector(".white-cover");
-        // background.classList.remove("active");
-      });
+    card.addEventListener("mouseenter", function () {
       let title = card.querySelector(".web-card-title");
       title.classList.add("active");
-      // let background = card.querySelector(".white-cover");
-      // background.classList.add("active");
 
-      const cardImages = document.querySelectorAll(".card-img");
-
-      cardImages.forEach(function (img) {
-        img.classList.remove("active");
-      });
       const img = card.querySelector(".card-img");
       img.classList.add("active");
+    });
+
+    card.addEventListener("mouseleave", function () {
+      let title = card.querySelector(".web-card-title");
+      title.classList.remove("active");
+
+      const img = card.querySelector(".card-img");
+      img.classList.remove("active");
     });
   });
 });
